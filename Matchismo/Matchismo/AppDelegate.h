6174715ae504fd1d8d4516c883151893e56714d7
jkairos <jkairos@gmail.com>
2014-10-18 15:59:00 -0300

    Added matchismo project to source control

---
+++ Matchismo/Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by José Ferreira de Souza Filho on 10/18/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by José Ferreira de Souza Filho on 10/18/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
